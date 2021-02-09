11c3ab831f3a4ff46204a38fccb8e4563e6c865c
jasimarif <jasimarifali@gmail.com>
2021-02-09 13:05:02 +0500

    post route added

--- standard/app.js
+++ standard/app.js
@@ -27,6 +27,41 @@ app.get('/webhooks', (req, res) => {
   res.status(200).send('Hello, webhooks!').end();
 });
 
+app.post("/webhooks", (req, res) => {
+  try {
+      console.log("/webhooks POST route hit! req.body: ", req.body)
+      postObject = req.body
+      req.headers['someHeader'] = 'someValue'
+      callSuitlelet(req.body)
+      res.sendStatus(200)
+
+}
+  catch (err) {
+      console.log("/webhooks route error: ", err)
+      res.sendStatus(404)
+  }
+})
+
+
+function  callSuitlelet(obj) {
+  const url="https://tstdrv1019523.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=792&deploy=1&compid=TSTDRV1019523&h=e9ce716184022a4abffb"
+  request({ url, json: true,
+      method:"POST",
+      body:obj,
+      // headers: {
+      //     "User-Agent": "Mozilla/5.0",
+      // }
+  }, 
+  (error, response,body) => {
+      if (error) {
+          console.log('Unable to connect to suitelet', body)
+      }
+      else{
+          console.log(`response ${response}`)
+      }
+  })
+
+
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
